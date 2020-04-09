13277fbdc627e1a8dab9e17c62fdb9fd197b6af5
pbailie <pbailie@users.noreply.github.com>
2020-04-09 13:19:12 -0400

    Refactor
    WIP

--- checkouts.php
+++ checkouts.php
@@ -1,14 +1,95 @@
 <?php
+require_once __DIR__ . "/common.php";
+require_once "HTML/Table.php";
+require_once 'DB.php';
 
-require_once(__DIR__."/common.php");
-print_header("License Checkouts");
+// First let's get license usage for the product specified in $feature
+// Connect to the database.  Use persistent connections.
+$db = DB::connect($dsn, true);
+if (DB::isError($db)) {
+	die ($db->getMessage());
+}
 
-?>
+$sql = <<<SQL
+SELECT DISTINCT `feature`
+FROM `events`
+WHERE `type`='OUT';
+SQL;
 
+$recordset = $db->query($sql);
 
+if (DB::isError($recordset)) {
+	die ($recordset->getMessage());
+}
+
+// Color code the features so it is easier to group them
+// Get a list of different colors
+$color = explode(",", $colors);
+for ($i = 0; $row = $recordset->fetchRow(); $i++) {
+    $features_color[$row[0]] = $color[$i];
+}
+
+// $i = 0;
+// while ($row = $recordset->fetchRow()) {
+//     $features_color["$row[0]"] = $color[$i];
+//     $i++;
+// }
+
+$sql = <<<SQL
+SELECT `date`, `user`, MAX(`feature`), count(*)
+FROM `events`
+WHERE `type`='OUT'
+GROUP BY `date`, `user`
+
+SQL;
+
+//Check what we want to sort data on
+if ($_GET['sortby'] == "date") {
+	$sql .= "ORDER BY `date`, `user`, `feature` DESC;";
+} else if ($_GET['sortby'] == "user" ) {
+	$sql .= "ORDER BY `user`, `date`, `feature` DESC;";
+} else {
+	$sql .= "ORDER BY `feature`, `date`, `user` DESC;";
+}
+
+if (isset($debug) && $debug == true)
+    print_sql($sql);
+
+$recordset = $db->query($sql);
+
+if (DB::isError($recordset)) {
+    die ($recordset->getMessage());
+}
+
+// Create a new table object
+$tableStyle = "border='1' cellpadding='1' cellspacing='2'";
+$table = new HTML_Table($tableStyle);
+
+$table->setColAttributes(1, "align='right'");
+
+// Define a table header
+$headerStyle = "style='background: yellow;'";
+$colHeaders = array("Date", "User", "Feature", "Total number of checkouts");
+$table->addRow($colHeaders, $headerStyle, "TH");
+
+$table->updateColAttributes(3, "align='enter'");
+
+// Right align the 3 column
+$table->updateColAttributes(2, "align='right'");
+
+// Add data rows to table
+while ($row = $recordset->fetchRow()) {
+    $table->AddRow($row, "style='background: {$features_color[$row[2]]};'");
+}
+
+$recordset->free();
+$db->disconnect();
+
+// View
+print_header();
+
+print <<< HTML
 <h1>License Checkouts</h1>
-
-
 <form>
 <p>Sort by
 <select onChange='this.form.submit();' name="sortby">
@@ -18,103 +99,8 @@ print_header("License Checkouts");
 </select>
 </p>
 </form>
-
-<?php
-
-##############################################################
-# We are using PHP Pear stuff ie. pear.php.net
-##############################################################
-require_once ("HTML/Table.php");
-require_once 'DB.php';
-
-$tableStyle = "border=\"1\" cellpadding=\"1\" cellspacing=\"2\"";
-
-# Create a new table object
-$table = new HTML_Table($tableStyle);
-
-$table->setColAttributes(1,"align=\"right\"");
-
-#  Define a table header
-$headerStyle = "style=\"background: yellow;\"";
-$colHeaders = array("Date", "User", "Feature", "Total number of checkouts");
-$table->addRow($colHeaders, $headerStyle, "TH");
-
-
-################################################################
-# First let's get license usage for the product specified in $feature
-##############################################################
-
-
-################################################################
-#  Connect to the database
-#   Use persistent connections
-################################################################
-$db = DB::connect($dsn, true);
-
-if (DB::isError($db)) {
-	die ($db->getMessage());
-}
-
-$sql = "SELECT DISTINCT `flmevent_feature` FROM `flexlm_events` WHERE `flmevent_type`='OUT'";
-
-$recordset = $db->query($sql);
-
-if (DB::isError($recordset)) {
-	die ($recordset->getMessage());
-}
-
-
-################################################################
-# I would like to color code my features so it is easier for me to group them :$
-# Get a list of different colors
-################################################################
-$color = explode(",", $colors);
-
-$i = 0;
-
-while ($row = $recordset->fetchRow()) {
-	 $features_color["$row[0]"] = $color[$i];
-	$i++;
-}
-
-################################################################
-# Check what we want to sort data on
-################################################################
-if ( $_GET['sortby'] == "date"){
-	$sql = "SELECT `flmevent_date`,`flmevent_user`,MAX(`flmevent_feature`),count(*) FROM `flexlm_events` WHERE `flmevent_type`='OUT' GROUP BY `flmevent_date`,`flmevent_user` ORDER BY `flmevent_date`,`flmevent_user`,`flmevent_feature` DESC;";
-}else if ( $_GET['sortby'] == "user" ){
-	$sql = "SELECT `flmevent_date`,`flmevent_user`,MAX(`flmevent_feature`),count(*) FROM `flexlm_events` WHERE `flmevent_type`='OUT' GROUP BY `flmevent_user`,`flmevent_date` ORDER BY `flmevent_user`,`flmevent_date`,`flmevent_feature` DESC;";
-}else{
-	$sql = "SELECT `flmevent_date`,MAX(flmevent_user),`flmevent_feature`,count(*) FROM `flexlm_events` WHERE `flmevent_type`='OUT' GROUP BY `flmevent_feature`,`flmevent_date` ORDER BY `flmevent_feature`,`flmevent_date`,`flmevent_user` DESC;";
-}
-
-if ( isset($debug) && $debug == 1 )
-              	print_sql ($sql);
-
-$recordset = $db->query($sql);
-
-if (DB::isError($recordset)) {
-    die ($recordset->getMessage());
-}
-
-while ($row = $recordset->fetchRow()) {
-    $table->AddRow($row, "style=\"background: " . $features_color[$row[2]]. ";\"");
-}
-
-
-$table->updateColAttributes(3,"align=\"center\"");
-
-$recordset->free();
-
-$db->disconnect();
-
-################################################################
-# Right align the 3 column
-################################################################
-$table->updateColAttributes(2,"align=\"right\"");
+HTML;
 
 $table->display();
-
-
+print_footer();
 ?>
-<?php print_footer(); ?>
